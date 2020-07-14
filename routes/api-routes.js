6c0b6edd02d834c6414d947dde4a44d108a5715c
AMA <alexanderabram6440@gmail.com>
2020-07-14 18:59:51 -0500

    created get and post for workouts within api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const db = require('../models');
 const router = express.Router();
+
+// Get Workouts
+router.get("/workouts", (req, res) => {
+    db.Workout.find({})
+        .then((dbWorkouts) => {
+            res.json(dbWorkouts);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// Post Workout
+router.post("/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
