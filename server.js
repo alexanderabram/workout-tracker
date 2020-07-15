0695c07ed1057eade5a47ec598a52e0d2e72285b
AMA <alexanderabram6440@gmail.com>
2020-07-14 19:09:54 -0500

    fixed issue with PORT and workout model link

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on port https://${PORT}!`);
+    console.log(`Running on port ${PORT}!`);
 });
